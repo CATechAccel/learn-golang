69d1813ae6019b815669caa5aa88a7c4b41485f4
sanae <sanae@MacBook-Air.local>
2020-11-17 15:13:57 +0900

    try2.goを編集（for,if,switchの利用）

--- step2/try2.go
+++ step2/try2.go
@@ -1,5 +1,22 @@
 package main
 
 func main() {
-	
+	//条件分岐にifを使用する場合
+	for i := 1; i <= 4; i = i+1{
+		if i%2 == 1 {
+			println("i - 奇数")
+		} else {
+			println("i - 偶数")
+		}
+	}
+
+	//条件分岐にswitchを使用する場合
+	for i:= 1; i <= 4; i += 1{
+		switch {
+		case i%2 == 1:
+			println("i - 奇数")
+		default:
+			println("i - 偶数")
+		}
+	}
 }

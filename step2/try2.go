1eefc735f1cf2122488c7045502bf5d5bbb325c9
sanae <sanae@MacBook-Air.local>
2020-11-17 15:41:12 +0900

    try2.goを編集

--- step2/try2.go
+++ step2/try2.go
@@ -1,12 +1,14 @@
 package main
 
+import "fmt"
+
 func main() {
 	//条件分岐にifを使用する場合
 	for i := 1; i <= 4; i = i+1{
 		if i%2 == 1 {
-			println("i - 奇数")
+			fmt.Printf("%d - 奇数\n", i)
 		} else {
-			println("i - 偶数")
+			fmt.Printf("%d - 偶数\n", i)
 		}
 	}
 
@@ -14,9 +16,9 @@ func main() {
 	for i:= 1; i <= 4; i += 1{
 		switch {
 		case i%2 == 1:
-			println("i - 奇数")
+			fmt.Printf("%d - 奇数\n", i)
 		default:
-			println("i - 偶数")
+			fmt.Printf("%d - 偶数\n",i)
 		}
 	}
 }

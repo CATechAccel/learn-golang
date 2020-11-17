fc35aa12a255a7de015402bf37b1ebac713a0ec1
sanae <sanae@MacBook-Air.local>
2020-11-17 16:09:38 +0900

    try3.goを編集

--- step2/try3.go
+++ step2/try3.go
@@ -1,5 +1,26 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
 
+func main() {
+	//現在時刻をナノ秒で表し変数に代入
+	t := time.Now().UnixNano()
+	//現在時刻を乱数の種として設定
+	rand.Seed(t)
+	s := rand.Intn(6) /*0-5の乱数*/
+
+	switch s + 1 {
+	case 1:
+		fmt.Println("凶")
+	case 2, 3:
+		fmt.Println("小吉")
+	case 4,5:
+		fmt.Println("中吉")
+	default:
+		fmt.Println("大吉")
+	}
 }

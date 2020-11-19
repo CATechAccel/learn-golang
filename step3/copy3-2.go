7141ebe850c30718e0dcf75492ef2ef7e38054ab
sne-5249 <sne.5249.k@gmail.com>
2020-11-19 23:04:03 +0900

    typeを使用した構造体の定義を追記

--- step3/copy3-2.go
+++ step3/copy3-2.go
@@ -2,14 +2,21 @@ package main
 
 import "fmt"
 
+/*
+typeを使用しての構造体の定義
+type user struct {
+	name string
+	age  int
+}
+*/
+
 func main() {
-	//構造体の定義
+	//構造体の定義 (typeを使用して型の定義を行うのが一般的)
 	p := struct {
 		name string
-		age int
+		age  int
 	}{name: "sanae", age: 20}
 
 	//フィールドの呼び出し
-	fmt.Println(p.name, p.age)  //sanae 20
+	fmt.Println(p.name, p.age) //sanae 20
 }
-

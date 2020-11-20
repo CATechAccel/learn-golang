4339ddb16d1ae144fad35c5ab965f3c3a0a20010
sne-5249 <sne.5249.k@gmail.com>
2020-11-20 13:29:44 +0900

    関数を要素として利用・for rangeの利用・無名関数

---
+++ step3/copy3-6.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+func print() string {
+	return "hoge"
+}
+
+func main() {
+	//配列の中に関数を要素として入れる
+	fs := make([]func() string, 2)
+	fs[0] = func() string { return "hoge" }
+	fs[1] = func() string { return "fuga" }
+	//for range スライス:第一返り値がindex番号、第２返り値が要素(value)
+	for i, f := range fs {
+		fmt.Println(i)
+		fmt.Println(f())
+	} //0 hoge 1 fuga
+
+	m := make(map[string]string)
+	m["name"] = "sanae"
+	m["birth"] = "5月29日"
+	//for range マップ:第一返り値がkey、第二返り値がvalue
+	for key, value := range m {
+		fmt.Println(key, value)
+	} // name sanae birth 5月29日
+
+	//無名関数:main関数の中で使用できる
+	func() {
+		fmt.Println("無名関数")
+	}() //無名関数
+}

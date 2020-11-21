362c58eef7525b13ec5891cbe476349d11b8abdc
sne-5249 <sne.5249.k@gmail.com>
2020-11-21 22:36:11 +0900

    参照したスライドのURLを追記

--- step4/try4-1/main.go
+++ step4/try4-1/main.go
@@ -1,5 +1,8 @@
 package main
 
+//4.1パッケージ　【try】パッケージを分けてみよう
+//https://docs.google.com/presentation/d/13ykFLzbQXgiHebsPtmvsov5Kqi7R0OnNxUxY6BZZ_dA/edit#slide=id.g4f1426e3ae_0_235
+
 import (
 	"fmt"
 

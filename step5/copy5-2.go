7a4bdefa03041a36ea8c9db5c43f537366ffd605
sne-5249 <sne.5249.k@gmail.com>
2020-11-26 23:46:32 +0900

    埋め込みの利用

---
+++ step5/copy5-2.go
@@ -0,0 +1,25 @@
+package main
+
+//6抽象化　埋め込みとフィールド
+//https://docs.google.com/presentation/d/1vKdJHHx4A_sP_Ft-3cakQqfJRbKxWSI9axzQ-GHHOoY/edit#slide=id.g4f15a7d687_0_284
+
+import "fmt"
+
+type Hoge struct {
+	N int
+}
+
+//埋め込み
+type Fuga struct {
+	Hoge
+}
+
+func main() {
+	f := Fuga{Hoge{N: 100}}
+	g := Hoge{N: 200}
+
+	fmt.Println(f.N)      //100
+	fmt.Println(f.Hoge.N) //100（型名を指定してアクセスすることもできる）
+	//埋め込みは継承ではない
+	fmt.Println(g.N) //200
+}

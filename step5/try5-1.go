c0d2e7fd3b5c40c92e61127b6aefae36a95c5f69
sne-5249 <sne.5249.k@gmail.com>
2020-11-27 00:22:38 +0900

    参照テキストのリンクを追記

--- step5/try5-1.go
+++ step5/try5-1.go
@@ -1,5 +1,8 @@
 package main
 
+//6.抽象化 【try】インターフェースを作ろう
+//https://docs.google.com/presentation/d/1vKdJHHx4A_sP_Ft-3cakQqfJRbKxWSI9axzQ-GHHOoY/edit#slide=id.g4f15a7d687_0_267
+
 import "fmt"
 
 //インターフェースを定義
